bca1e877b961eda57829a1c725b90a0d93986f56
SaturdayAM <jwl62@georgetown.edu>
2018-05-21 15:46:18 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,12 @@
+import React from 'react';
 // Code SimplerComponentHere Here
+const SimplerComponent = (props) =>{
+	return(
+		<div style={{margin: 10, border: "1px solid red"}}
+			  onClick={props.handleClick}>
+			  <p>I am just happy</p>
+		</div>
+	)
+}
+
+export default SimplerComponent;
